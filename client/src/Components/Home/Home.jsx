e1b6b10e86f0ad02916a0016477a29a5ec2e022f
iMrihan <mhd.khan.rihan@gmail.com>
2022-03-29 20:05:02 +0530

    home component added

---
+++ client/src/Components/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Footer from "./Footer";
+import Header from "./Header";
+import Main from "./Main";
+
+export default function Home() {
+	return (
+		<div>
+			<Header />
+			<Main />
+			<Footer />
+		</div>
+	);
+}
